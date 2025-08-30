5743b317e37c7316f50b9a75fed899f4e66fe6c5
Soh <columbine.k@gmail.com>
2025-08-30 21:38:03 +0800

    add side router config

--- scripts/init-settings.sh
+++ scripts/init-settings.sh
@@ -11,4 +11,15 @@ uci commit luci
 # uci set fstab.@global[0].check_fs=1
 # uci commit fstab
 
+
+# Configure NanoPi R5S as a side router
+uci set network.lan.ipaddr='192.168.0.100'
+uci set network.lan.proto='static'
+uci set network.lan.type='bridge'
+uci set network.lan.ifname='eth1'
+uci set network.lan.netmask='255.255.255.0'
+uci set network.lan.gateway='192.168.0.1'
+uci set network.lan.dns='192.168.0.1'
+uci commit network
+
 exit 0
